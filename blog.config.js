f1e8b4f008eb998115b15dcc093d9a1743ca4ee4
thefullhorizon <thefullhorizon@gmail.com>
2026-03-22 18:17:31 +0800

    SEO

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Taylor', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '通过Data和AI，帮助中小企业和个人实现持续的商业价值。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://taylorai.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Commerce、AI、Data、Crypto、科技、商业', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Commerce,AI,Data,Crypto,科技,商业', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
